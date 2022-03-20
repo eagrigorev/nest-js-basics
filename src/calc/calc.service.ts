b9c9c66bc6ee6ffc574075a1b8b66370064ff76e
Evgenii Grigorev <eagrigorev87@gmail.com>
2022-03-20 18:57:40 +0300

    Домашняя работа №1
    Сделал все задания по лекции и методичке. Делал без базы данных чтобы не нарушать логику курса. В калькуляторе добавил дополнительно возможность посмотреть историю запросов по эндпоинту calc/log.

---
+++ src/calc/calc.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Calc } from './dto/calc.dto';
+
+const calc: Calc[] = [];
+
+@Injectable()
+export class CalcService {
+  private readonly calc: Calc[] = [];
+
+  async log(): Promise<Calc[]> {
+    return calc;
+  }
+
+  async summ(data: Calc): Promise<string> {
+    const result = data.num1 + data.num2;
+    const operationEntry = { ...data, type: 'summ', result: result };
+    calc.push(operationEntry);
+    return `Сумма двух чисел равна ${result}`;
+  }
+
+  async diff(data: Calc): Promise<string> {
+    const result = data.num1 - data.num2;
+    const operationEntry = { ...data, type: 'diff', result: result };
+    calc.push(operationEntry);
+    return `Разность двух чисел равна ${result}`;
+  }
+
+  async mult(data: Calc): Promise<string> {
+    const result = data.num1 * data.num2;
+    const operationEntry = { ...data, type: 'mult', result: result };
+    calc.push(operationEntry);
+    return `Произведение двух чисел равно ${result}`;
+  }
+
+  async div(data: Calc): Promise<string> {
+    const result = data.num1 / data.num2;
+    const operationEntry = { ...data, type: 'div', result: result };
+    calc.push(operationEntry);
+    return `Частное от деления двух чисел равно ${result}`;
+  }
+}
---
+++ src/calc/dto/calc.dto.ts
@@ -0,0 +1,6 @@
+export class Calc {
+  type!: string;
+  num1!: number;
+  num2!: number;
+  result!: number;
+}
